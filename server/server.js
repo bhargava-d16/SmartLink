ad4d37d31e60b0688b1d0b91bdc270ee9e70f84f
bhargava-d16 <bsdabhade_b23@et.vjti.ac.in>
2025-09-24 03:24:40 +0530

    added analytics

--- server/server.js
+++ server/server.js
@@ -13,6 +13,9 @@ dotenv.config();
 const PORT = process.env.PORT || 8080;
 const app = express();
 
+app.get('/health', (req, res) => {
+  res.status(200).json({ status: 'OK' });
+});
 app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ limit: "5mb", extended: true }));
 app.use(cookieParser());
@@ -48,7 +51,7 @@ const startServer = async () => {
     await connectDB();
     console.log("MongoDB Connection Ready");
 
-    app.listen(PORT, () => {
+    app.listen(PORT, '0.0.0.0',() => {
       console.log(` Server running on port ${PORT}`);
     });
   } catch (error) {
