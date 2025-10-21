8b18e4daaaaf6be9d2fcb4f7653844dff0d44e45
bhargava-d16 <bsdabhade_b23@et.vjti.ac.in>
2025-10-21 12:00:26 +0530

    deploying

--- client/src/libs/axios.jsx
+++ client/src/libs/axios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL:"https://smartlink-fbbw.onrender.com",
+  baseURL:"https://smartlink-fbbw.onrender.com/",
   // baseURL:"http://localhost:8080/api/",
   withCredentials: true,
 });
